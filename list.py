0b096b07b4fe817557720f4ceb23548c26a9a977
Vaishnavi <102135930+abbugarivaishnavi@users.noreply.github.com>
2022-07-15 04:38:52 -1200

    Add files via upload

---
+++ list.py
@@ -0,0 +1,105 @@
+import logging
+
+logging.basicConfig(filename="list.log",level = logging.DEBUG,format=("%(name)s %(levelname)s %(asctime)s %(message)s"))
+
+class list:
+    def __init__(self, l):
+        self.l = l
+
+    def reverse_list(self,l):
+        """this function helps in reversing the whole list"""
+        try:
+            val = self.l[::-1]
+            logging.info(val)
+            return val
+        except Exception as e:
+            logging.error(e)
+
+    def accesing_234(self,l):
+       """this func helps in accesing 234"""
+       output_list=[]
+       try:
+           for i  in self.l:
+               if type(i)==list or type(i)==tuple:
+                   for j in i:
+                       if type(j)==int:
+                           if j == 234:
+                               output_list.append(j)
+               if type(i)==dict:
+                   for k,v in i.items():
+                       if k == int:
+                           if k == 234:
+                               output_list.append(k)
+           logging.info(output_list)
+       except Exception as e:
+           logging.error(e)
+
+    def access_456_in_data(self,l):
+       '''This function access 456'''
+       dummy_list = []
+       try:
+           for i in self.l:
+               if type(i) == list or type(i) == tuple:
+                   for j in i:
+                       if type(j) == int:
+                           if j == 456:
+                               dummy_list.append(j)
+               if type(i) == dict:
+                   for k, v in i.items():
+                       if type(k) == int:
+                           if k == 456:
+                               dummy_list.append(k)
+           logging.info(dummy_list)
+       except Exception as e:
+            logging.exception(e)
+
+    def extracting_sudh(self,l):
+        """helps in extracting sudh from list entity"""
+        dummy_list=[]
+        try:
+            for i in self.l:
+                if type(i)==list or type(i)==tuple:
+                    for j in i :
+                        if type(j)!= int :
+                            if j == "sudh":
+                                dummy_list.append(j)
+                if type(i)==dict:
+                    for k,v in i.items():
+                        if type(k) != int or type(v) != int:
+                            if k == "sudh" or v == "sudh":
+                                dummy_list.append(v)
+            logging.info(dummy_list)
+        except Exception as e:
+            logging.error(e)
+
+    def extracting_values(self,l):
+        """extracting values from dict entity"""
+        dummy_list=[]
+        try:
+            for i in self.l:
+                if type(i)==dict:
+                    for k,v in i.items():
+                        dummy_list.append(v)
+            logging.info(dummy_list)
+        except Exception as e:
+            logging.error(e)
+
+    def extracting_keys(self,l):
+        """extracting keys frm dict entity"""
+        dummy_list=[]
+        try:
+            for i in self.l:
+                if type(i)==dict:
+                    for k , v in i.items():
+                        dummy_list.append(k)
+            logging.info(dummy_list)
+        except Exception as e:
+            logging.error(e)
+l = [3,4,5,6,7 , [23,456,67,8,78,78] , [345,56,87,8,98,9] , (234,6657,6) , {"key1" :"sudh" , 234:[23,45,656]}]
+obj = list(l)
+obj.reverse_list(l)
+obj.accesing_234(l)
+obj.access_456_in_data(l)
+obj.extracting_sudh(l)
+obj.extracting_keys(l)
+obj.extracting_values(l)
